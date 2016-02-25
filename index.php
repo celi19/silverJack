82c82a1c8d2f4de6deb7ed42fed161444565a878
sanc8629 <lesanchez@csumb.edu>
2016-02-25 06:55:03 +0000

    added html format to php file

--- index.php
+++ index.php
@@ -15,19 +15,7 @@ function displayWinners(){
     <head>
         <title>SilverJack</title>
         <link href="css/styles.css" rel="stylesheet" />
-        <style>
-        @-webkit-keyframes mymove {
-            from {background-color: red;}
-            to {background-color: blue;}
-            
-        }
-        /* Standard syntax */
-        @keyframes mymove {
-            from {background-color: red;}
-            to {background-color: blue;}
-            
-        }
-        </style>
+
         
         <h1>SilverJack</h1>
     </head>
