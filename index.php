b30468e1552adb5def50bfd9941cc6d572042359
sanc8629 <lesanchez@csumb.edu>
2016-02-25 06:52:40 +0000

    added functions to php

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
 <?php
+function getHand(){
+    
+}
+function displayHand(){
+    
+}
+function displayWinners(){
+    
+}
 
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SilverJack</title>
+        <link href="css/styles.css" rel="stylesheet" />
+        <style>
+        @-webkit-keyframes mymove {
+            from {background-color: red;}
+            to {background-color: blue;}
+            
+        }
+        /* Standard syntax */
+        @keyframes mymove {
+            from {background-color: red;}
+            to {background-color: blue;}
+            
+        }
+        </style>
+        
+        <h1>SilverJack</h1>
+    </head>
+    <body>
+       
+
+    </body>
+</html>
